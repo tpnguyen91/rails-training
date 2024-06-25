7e9a6545df3375ba56cfaaa5b0c85ec8c6334f6c
NguyenPhuongfreeC <tpnguyen91@gmail.com>
2024-06-25 17:00:48 +0700

    1st commit

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,24 @@
+class UsersController < ApplicationController
+  skip_before_action :authorized, only: [:create]
+  rescue_from ActiveRecord::RecordInvalid, with: :handle_invalid_record
+
+  def create 
+      user = User.create!(user_params)
+      @token = encode_token(user_id: user.id)
+      render json: {
+          user: UserSerializer.new(user), 
+          token: @token
+      }, status: :created
+  end
+
+  private
+
+    def user_params 
+        params.permit(:email, :password_digest, :full_name)
+    end
+
+    def handle_invalid_record(e)
+            render json: { errors: e.record.errors.full_messages }, status: :unprocessable_entity
+    end
+
+end
